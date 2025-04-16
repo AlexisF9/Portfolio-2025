858b47cd620cf37a7a743efe6cfbd582c866fb01
Alexis Flacher <alexis.flacher38@gmail.com>
2025-04-16 03:46:12 +0200

    edit not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,17 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-6">
-      <h1 className="text-4xl font-bold mb-4">404 - Page introuvable</h1>
+      <h1 className="font-gothic text-4xl font-bold mb-4">
+        404 - Page introuvable
+      </h1>
       <p className="text-lg mb-6">
         Désolé, la page que vous recherchez n&apos;existe pas.
       </p>
-      <Link href="/">
-        <button>Retour à l&apos;accueil</button>
+      <Link
+        className="block font-gothic w-fit border border-transparent rounded-full py-2 px-4 bg-linear-to-r hover:from-transparent hover:to-transparent hover:dark:text-white hover:border-neutral-600 hover:dark:border-white from-react to-vue transition duration-300 ease-in-out"
+        href={"/"}
+      >
+        Retour à l&apos;accueil
       </Link>
     </div>
   );
