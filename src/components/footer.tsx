42d36ec2f34fcb161dada1e6eb1a141acce3e34d
AlexisF9 <alexis.flacher38@gmail.com>
2025-06-24 11:15:50 +0200

    edit footer style in mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,22 +10,26 @@ export function Footer() {
   const footer = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
-    gsap.fromTo(
-      footer.current,
-      { opacity: 0, width: "50%" },
-      {
-        opacity: 1,
-        width: "100%",
-        duration: 1,
-        ease: "power3.out",
-        scrollTrigger: {
-          trigger: footer.current,
-          start: "top bottom",
-          end: "bottom bottom",
-          markers: false,
-        },
-      }
-    );
+    ScrollTrigger.matchMedia({
+      "(min-width: 768px)": () => {
+        gsap.fromTo(
+          footer.current,
+          { opacity: 0, width: "50%" },
+          {
+            opacity: 1,
+            width: "100%",
+            duration: 1,
+            ease: "power3.out",
+            scrollTrigger: {
+              trigger: footer.current,
+              start: "top bottom",
+              end: "bottom bottom",
+              markers: false,
+            },
+          }
+        );
+      },
+    });
 
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
@@ -35,7 +39,7 @@ export function Footer() {
   return (
     <footer
       ref={footer}
-      className="flex flex-col align-center gap-2 items-center justify-center opacity-0 w-full mx-auto my-20 py-16 px-9 bg-neutral-800 rounded-full dark:bg-white"
+      className="flex flex-col align-center gap-2 items-center justify-center md:opacity-0 w-full mx-auto mt-20 md:my-20 py-16 px-9 bg-neutral-800 rounded-t-4xl md:rounded-full dark:bg-white"
     >
       <p className="w-max font-gothic text-xl md:text-4xl text-white dark:text-neutral-950">
         Alexis Flacher
