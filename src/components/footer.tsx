2ed802eb6a260228d4a9c891a6616119f3e5264f
AlexisF9 <alexis.flacher38@gmail.com>
2025-06-25 09:44:28 +0200

    edit footer padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -39,7 +39,7 @@ export function Footer() {
   return (
     <footer
       ref={footer}
-      className="flex flex-col align-center gap-2 items-center justify-center md:opacity-0 w-full mx-auto mt-20 md:mb-8 py-16 px-9 bg-neutral-800 rounded-t-4xl md:rounded-full dark:bg-white"
+      className="flex flex-col align-center gap-2 items-center justify-center md:opacity-0 w-full mx-auto mt-20 md:mb-8 py-12 px-9 bg-neutral-800 rounded-t-4xl md:rounded-full dark:bg-white"
     >
       <p className="w-max font-gothic text-xl md:text-4xl text-white dark:text-neutral-950">
         Alexis Flacher
