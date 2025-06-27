135cdee94447c8a2cce5a8a41fef0932f2711005
AlexisF9 <alexis.flacher38@gmail.com>
2025-06-27 11:53:28 +0200

    remove import

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Image from "next/image";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
